32ad9abed894c8e897920cb9095c1cf8de18e724
@SWJC@ <simonclayden@hotmail.co.uk>
2011-05-30 13:09:19 +0100

    branch_1.2 DIVS added

---
+++ index.php
@@ -0,0 +1,23 @@
+<style type="text/css">
+<!--
+#wrapper {
+	height: 500px;
+	width: 1000px;
+}
+#header {
+	height: 200px;
+	width: 1000px;
+	top: auto;
+}
+#footer {
+	height: 30px;
+	width: 1000px;
+	bottom: 1%;
+}
+-->
+</style>
+<div id="wrapper">
+  <div id="header">
+    <div id="footer">Content for  id "footer" Goes Here</div>
+  </div>
+</div>
